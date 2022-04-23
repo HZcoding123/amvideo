d16835589d735897196ebef0a2e08fbc7f2b6b1c
HZcoding123 <975955434@qq.com>
2022-04-23 12:05:43 +0800

    修改了数据库配置

--- amvideo/settings/dev.py
+++ amvideo/settings/dev.py
@@ -86,10 +86,17 @@ WSGI_APPLICATION = 'amvideo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'amvideo',
+        'USER': 'amvideo',
+        'PASSWORD': 'amvideo123',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
+
+import pymysql
+pymysql.install_as_MySQLdb()
 
 
 # Password validation
